7a390dbdb0c1024aa4e85a85999cbf9fa7b47400
Zitle-Nancy <ancizj@hotmail.com>
2017-11-25 10:38:34 -0600

    agregaga validacion en las tarjetas

--- js/app.js
+++ js/app.js
@@ -1,7 +1,9 @@
 (function(){
 	var radioButton = document.getElementsByClassName('radio');
 	var longitud = radioButton.length;
-
+	var inputCard = document.getElementById('input-card');
+	inputCard.addEventListener('keyup',validateCardNumber);
+	inputCard.addEventListener('keydown',validateNumber);
 	for (var i = 0; i < longitud; i++) {
 		radioButton[i].addEventListener('change', getFormCard);
 	};
@@ -15,6 +17,30 @@
 			formCard.classList.add('hidden');  
 		}
 	};
+	function validateNumber(e){
+		if (e.keyCode !== 8 && (e.keyCode < 48 || e.keyCode > 57)){
+			e.preventDefault();
+		}	
+	};
+	function validateCardNumber(){
+		var regCardVisa = /^4\d{3}-?\d{4}-?\d{4}-?\d{4}$/;
+		var regMasterCard = /^5[1-5]\d{2}-?\d{4}-?\d{4}-?\d{4}$/;
+		var numberCard = this.value;
+		var cardVisa = document.getElementById('card-visa');
+		var cardMaster = document.getElementById('master-card');
+		if (regCardVisa.test(numberCard)){
+			cardVisa.classList.remove('hidden');
+		} else {
+			cardVisa.classList.add('hidden');
+		}
+		if (regMasterCard.test(numberCard)){
+			cardMaster.classList.remove('hidden');
+		} else{
+			cardMaster.classList.add('hidden');
+		}
+		// console.log(this.value);
+	};
 	
+
 
 })();
