a73eb1b28de095d226a47c353c9d7240a47e91bb
Zitle-Nancy <ancizj@hotmail.com>
2017-11-25 07:52:02 -0600

    agrega funcion de mostrar u ocultar el formulario de card

--- js/app.js
+++ js/app.js
@@ -1,4 +1,20 @@
 (function(){
+	var radioButton = document.getElementsByClassName('radio');
+	var longitud = radioButton.length;
+
+	for (var i = 0; i < longitud; i++) {
+		radioButton[i].addEventListener('change', getFormCard);
+	};
+	
+	function getFormCard() {
+		var formCard = document.getElementById('form-card');
+
+		if (this.id === 'card' && this.checked) {
+			formCard.classList.remove('hidden');  
+		} else {
+			formCard.classList.add('hidden');  
+		}
+	};
 	
 
 })();
